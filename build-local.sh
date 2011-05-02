ebbdac5002220270e77676b9bc8c7d08ca5a6cc6
kabum <uu.kabum@gmail.com>
2011-05-02 10:35:52 +0200

    remove obsolete scripts

--- build-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while { inotifywait -e modify -e close_write -e create -e move -e delete -r backend-php/; }; do
-    make local_dev
-done
-
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-while { inotifywait -e modify -e close_write -e create -e move -e delete -r --fromfile files.txt; }; do
-	make test
-done
